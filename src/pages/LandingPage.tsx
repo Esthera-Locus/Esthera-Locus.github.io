8a39e3ed6e3978842f7f18f9118933d942aaaf2a
damarhtny <157679236+pabsss53@users.noreply.github.com>
2026-01-09 05:54:40 +0700

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-export default function LandingPage() {
+import { createRoot } from 'react-dom/client';
 import { useRef, useEffect, useState } from "react";
 import { motion , AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
